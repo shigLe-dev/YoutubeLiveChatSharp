d11a385bfdbaea0a851daba54166232c135e336b
KYapp <kukiyomenaisapp@gmail.com>
2022-12-04 16:44:53 +0900

    Init

---
+++ Test/Program.cs
@@ -0,0 +1,10 @@
+﻿using YoutubeLiveChatSharp;
+
+ChatFetch f = new ChatFetch("jfKfPfyJRdk");
+
+while (true)
+{
+    f.Fetch().ToList().ForEach(c => Console.WriteLine(c.text));
+
+    await Task.Delay(1000);
+}
