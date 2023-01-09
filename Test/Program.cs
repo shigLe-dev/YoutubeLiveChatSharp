5df471f4d693e0641cf527e8ff7dad8c12d27bbb
KYapp <kukiyomenaisapp@gmail.com>
2023-01-09 21:55:48 +0900

    コメント追加

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,9 @@ ChatFetch f = new ChatFetch("jfKfPfyJRdk");
 
 while (true)
 {
+    // 取得して表示
     f.Fetch().ToList().ForEach(c => Console.WriteLine(c.text));
 
+    // 負荷を掛けすぎないようにちょっと待つ
     await Task.Delay(1000);
 }
